c8ef2da293a123a85e7888f22823a845cbf6c202
Linh PD <linhpd@elinext.com>
2015-02-10 17:15:30 +0700

    testing with gitignore

--- html/js/controllers/gridCtrl.js
+++ html/js/controllers/gridCtrl.js
@@ -2,7 +2,7 @@
 * @Author: UnS
 * @Date:   2015-02-10 14:52:21
 * @Last Modified by:   UnS
-* @Last Modified time: 2015-02-10 17:05:52
+* @Last Modified time: 2015-02-10 17:08:25
 */
 
 define(['app'], function(app) {
@@ -10,7 +10,7 @@ define(['app'], function(app) {
 		$scope.gridOptions = {
 			enableHorizontalScrollbar : 0,
 			enableVerticalScrollbar   : 2,
-			enableSorting             : false,	
+			enableSorting             : true,	
 			columnDefs                :[
 				{field: "firstName"},
 				{field: "lastName"},
